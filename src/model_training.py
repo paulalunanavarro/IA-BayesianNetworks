fa15873dcb30cba86bd9e5799545bc74e85a492a
PAULA LUNA <paulunnav@alum.us.es>
2024-06-07 13:17:52 +0200

    construccion de la red bow-tie

--- src/model_training.py
+++ src/model_training.py
@@ -1,107 +1,69 @@
 import pandas as pd
 from pgmpy.models import BayesianNetwork
-from pgmpy.factors.discrete import TabularCPD
 from pgmpy.estimators import MaximumLikelihoodEstimator
-from pgmpy.inference import VariableElimination
+
+
 
 data = pd.read_csv("./data/natural_disasters_extended.csv")
 
 
+
 # Definir la estructura de la red bow-tie con al menos 10 nodos
-model = BayesianNetwork([
-    ('CentralNode', 'ResponseTime'),
-    ('CentralNode', 'ResponseDuration'),
-    ('CentralNode', 'PeopleAffected'),
-    ('CentralNode', 'PeopleAssisted'),
-    ('CentralNode', 'InfrastructureCondition'),
-    ('CentralNode', 'PreparednessTraining'),
-    ('CentralNode', 'CommunityAwareness'),
-    ('CentralNode', 'AgenciesInvolved'),
-    ('CentralNode', 'CoordinationScore'),
-    ('CentralNode', 'CommunicationScore'),
-    ('CentralNode', 'EconomicDamage'),
-    ('CentralNode', 'Casualties'),
-    ('CentralNode', 'LongTermImpact')
+model = BayesianNetwork() # se crea una instancia de la clase BayesianNetwork de pgmpy. Esta clase representa el modelo de red bayesiana que estamos construyendo.
+
+model.add_edges_from([ #cada tupla representa una conexion entre dos nodos
+    #el nodo central es DisasterType
+    #Esto son las causas o factores que influyen en el tipo de desastre
+    ("PreparednessTraining", "DisasterType"),
+    ("CommunityAwareness", "DisasterType"),
+    ("AgenciesInvolved", "DisasterType"),
+    ("CoordinationScore", "DisasterType"),
+    ("CommunicationScore", "DisasterType"),
+    #Consecuencias o impactos que tienen diferentes aspectos del desastre
+    ("DisasterType", "Personnel"),
+    ("DisasterType", "MedicalSupplies"),
+    ("DisasterType", "Shelters"),
+    ("DisasterType", "FoodWater"),
+    ("DisasterType", "ResponseTime"),
+    ("DisasterType", "ResponseDuration"),
+    ("DisasterType", "PeopleAffected"),
+    ("DisasterType", "PeopleAssisted"),
+    ("DisasterType", "InfrastructureCondition"),
+    ("DisasterType", "EconomicDamage"),
+    ("DisasterType", "Casualties"),
+    ("DisasterType", "LongTermImpact")
 ])
 
-# Define CPDs para los nodos restantes en función de sus padres en el grafo
-
-# CPDs para los nodos principales
-
-# Rellenar los CPDs restantes con los valores adecuados
-num_central_nodes = 4
-
-cpd_central_node = TabularCPD(variable='CentralNode', variable_card=num_central_nodes,
-                               values=[[0.25], [0.25], [0.25], [0.25]])
-
-cpd_response_time = TabularCPD(variable='ResponseTime', variable_card=2,
-                                values=[[0.7, 0.3],
-                                        [0.6, 0.4]],
-                                evidence=['CentralNode'], evidence_card=[num_central_nodes])
-
-cpd_response_duration = TabularCPD(variable='ResponseDuration', variable_card=2,
-                                    values=[[0.8, 0.2],
-                                            [0.3, 0.7]],
-                                    evidence=['CentralNode'], evidence_card=[num_central_nodes])
- 
-cpd_people_affected = TabularCPD(variable='PeopleAffected', variable_card=2,
-                                  values=[[0.7, 0.3], [0.3, 0.7]],
-                                  evidence=['CentralNode'], evidence_card=[2])
-
-cpd_people_assisted = TabularCPD(variable='PeopleAssisted', variable_card=2,
-                                  values=[[0.6, 0.7, 0.4, 0.3],
-                                          [0.4, 0.3, 0.6, 0.7]],
-                                  evidence=['CentralNode', 'ResponseTime'], evidence_card=[2, 2])
-
-cpd_infrastructure_condition = TabularCPD(variable='InfrastructureCondition', variable_card=2,
-                                          values=[[0.4, 0.3, 0.3],
-                                                  [0.3, 0.4, 0.3]],
-                                          evidence=['CentralNode'], evidence_card=[2])
-
-cpd_preparedness_training = TabularCPD(variable='PreparednessTraining', variable_card=2,
-                                       values=[[0.5, 0.5], [0.2, 0.8]],
-                                       evidence=['CentralNode'], evidence_card=[2])
-
-cpd_community_awareness = TabularCPD(variable='CommunityAwareness', variable_card=2,
-                                     values=[[0.4, 0.6], [0.3, 0.7]],
-                                     evidence=['CentralNode'], evidence_card=[2])
-
-cpd_agencies_involved = TabularCPD(variable='AgenciesInvolved', variable_card=2,
-                                   values=[[0.6, 0.4], [0.4, 0.6]],
-                                   evidence=['CentralNode'], evidence_card=[2])
-
-cpd_coordination_score = TabularCPD(variable='CoordinationScore', variable_card=2,
-                                    values=[[0.7, 0.3], [0.4, 0.6]],
-                                    evidence=['CentralNode'], evidence_card=[2])
-
-cpd_communication_score = TabularCPD(variable='CommunicationScore', variable_card=2,
-                                     values=[[0.8, 0.2], [0.3, 0.7]],
-                                     evidence=['CentralNode'], evidence_card=[2])
-
-cpd_economic_damage = TabularCPD(variable='EconomicDamage', variable_card=2,
-                                  values=[[0.8, 0.2], [0.3, 0.7]],
-                                  evidence=['CentralNode'], evidence_card=[2])
-
-cpd_casualties = TabularCPD(variable='Casualties', variable_card=2,
-                             values=[[0.7, 0.3], [0.3, 0.7]],
-                             evidence=['CentralNode'], evidence_card=[2])
-
-cpd_long_term_impact = TabularCPD(variable='LongTermImpact', variable_card=2,
-                                   values=[[0.8, 0.2], [0.3, 0.7]],
-                                   evidence=['CentralNode'], evidence_card=[2])
-
-# Agregar todas las CPDs al modelo
-model.add_cpds(cpd_central_node, cpd_response_duration, cpd_response_time,cpd_people_affected, cpd_people_assisted, cpd_infrastructure_condition,
-                cpd_preparedness_training, cpd_community_awareness, cpd_agencies_involved,
-                cpd_coordination_score, cpd_communication_score, cpd_economic_damage,
-                cpd_casualties, cpd_long_term_impact)
+# Estimar las probabilidades condicionales a partir de los datos
+estimator = MaximumLikelihoodEstimator(model, data)
+cpd_preparedness_training = estimator.estimate_cpd('PreparednessTraining')
+cpd_community_awareness = estimator.estimate_cpd('CommunityAwareness')
+cpd_agencies_involved = estimator.estimate_cpd('AgenciesInvolved')
+cpd_coordination_score = estimator.estimate_cpd('CoordinationScore')
+cpd_communication_score = estimator.estimate_cpd('CommunicationScore')
+cpd_disaster_type = estimator.estimate_cpd('DisasterType')
+cpd_personnel = estimator.estimate_cpd('Personnel')
+cpd_medical_supplies = estimator.estimate_cpd('MedicalSupplies')
+cpd_shelters = estimator.estimate_cpd('Shelters')
+cpd_food_water = estimator.estimate_cpd('FoodWater')
+cpd_response_time = estimator.estimate_cpd('ResponseTime')
+cpd_response_duration = estimator.estimate_cpd('ResponseDuration')
+cpd_people_affected = estimator.estimate_cpd('PeopleAffected')
+cpd_people_assisted = estimator.estimate_cpd('PeopleAssisted')
+cpd_infrastructure_condition = estimator.estimate_cpd('InfrastructureCondition')
+cpd_economic_damage = estimator.estimate_cpd('EconomicDamage')
+cpd_casualties = estimator.estimate_cpd('Casualties')
+cpd_long_term_impact = estimator.estimate_cpd('LongTermImpact')
 
 
-# Comprobar si el modelo es válido
+# Agregar CPDs al modelo
+model.add_cpds(
+    cpd_preparedness_training, cpd_community_awareness, cpd_agencies_involved, cpd_coordination_score,
+    cpd_communication_score, cpd_disaster_type, cpd_personnel, cpd_medical_supplies, cpd_shelters,
+    cpd_food_water, cpd_response_time, cpd_response_duration, cpd_people_affected, cpd_people_assisted,
+    cpd_infrastructure_condition, cpd_economic_damage, cpd_casualties, cpd_long_term_impact
+)
+
+# Comprobar si el modelo está correctamente definido
 assert model.check_model()
-
-
-
-
-
 
